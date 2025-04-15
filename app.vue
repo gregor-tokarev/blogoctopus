0c0591ff47561995f2bf4fa4be40f49629403184
gregortokarev <tokarev.egor3@wildberries.work>
2025-04-15 07:54:19 +0300

    feat: add google oauth

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+import {authClient} from "~/lib/auth-client";
+
+async function signIn(): Promise<void> {
+  const data = await authClient.signIn.social({
+    provider: "google"
+  })
+
+  console.log(data)
+}
+
+</script>
+
 <template>
-    <div>
-        <h1 class="text-9xl">title</h1>
-    </div>
+  <div>
+    <h1 class="text-9xl">title</h1>
+    <button @click="signIn">google</button>
+  </div>
 </template>
--- server/better-auth.ts
+++ server/better-auth.ts
@@ -13,4 +13,11 @@ export const auth = betterAuth({
   }),
   secret: process.env.BETTER_ATUH_SECRET,
   url: process.env.BETTER_AUTH_URL,
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_AUTH_SECRET
+    }
+  }
 });
