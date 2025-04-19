43e62abbda3c70083c36282ce4f43be7a3090220
gregortokarev <tokarev.egor3@wildberries.work>
2025-04-19 10:52:50 +0300

    feat(dashboard): add sidebar with menu items

---
+++ components/dashboard/AppSidebar.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { Cable, Clock, Plus } from "lucide-vue-next"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarMenuAction
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+    {
+        title: "Интеграции",
+        url: "#",
+        icon: Cable,
+    },
+    {
+        title: "Запланированные",
+        url: "#",
+        icon: Clock,
+    },
+];
+</script>
+
+<template>
+    <Sidebar>
+        <SidebarContent>
+            <SidebarGroup>
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        <SidebarMenuItem v-for="item in items" :key="item.title">
+                            <SidebarMenuButton asChild>
+                                <a :href="item.url">
+                                    <component :is="item.icon" />
+                                    <span>{{ item.title }}</span>
+                                </a>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+            <SidebarGroup>
+                <SidebarGroupContent>
+                    <UiButton size="xs" variant="default" class="w-full">
+                        <Plus />Новая публикация
+                    </UiButton>
+                </SidebarGroupContent>
+            </SidebarGroup>
+        </SidebarContent>
+    </Sidebar>
+</template>
---
+++ pages/dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <SidebarTrigger />
+      <NuxtPage />
+    </main>
+  </SidebarProvider>
+</template>
+
+<script setup lang="ts">
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import AppSidebar from "@/components/dashboard/AppSidebar.vue"
+</script>
