68f662f0dca065cd9a7ccb816b2767b419878adb
gregortokarev <tokarev.egor3@wildberries.work>
2025-05-21 06:59:59 +0300

    fix: export default task

--- server/trigger/schedule-post.ts
+++ server/trigger/schedule-post.ts
@@ -1,6 +1,6 @@
 import { logger, task } from "@trigger.dev/sdk/v3";
 
-const schedulePostTask = task({
+export const schedulePostTask = task({
     id: "schedule-post",
     run: async (payload: any, { ctx }) => {
         logger.info("Hello, world!", { payload, ctx });
